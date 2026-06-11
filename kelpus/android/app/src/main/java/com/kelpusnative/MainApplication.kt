2afa53cc318ef1fdd18ada7bfd3741bea5706bfc
1mn2147 <wed1234555@gmail.com>
2026-06-11 11:38:52 +0900

    hotfix: app build package added

--- kelpus/android/app/src/main/java/com/kelpusnative/MainApplication.kt
+++ kelpus/android/app/src/main/java/com/kelpusnative/MainApplication.kt
@@ -5,6 +5,7 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
@@ -14,7 +15,7 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages() =
+        override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here.
             }
